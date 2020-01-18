1e3a91b7a93f78e4e9398830e878181a9bd03621
Dante Tobar <dante.m.tobar@gmail.com>
2020-01-18 11:15:47 -0800

    server responds to categories request

--- server/api/category.js
+++ server/api/category.js
@@ -1,17 +1,25 @@
-const router = require("express").Router();
-const { Category, Nominee, Movie } = require("../db");
+const router = require('express').Router();
+const { Category, Nominee, Movie } = require('../db');
 
-router.get("/", (req, res) => {
-  Category.findAll({
-    include: [
-      {
-        model: Nominee,
-        include: [
-          {
-            model: Movie
-          }
-        ]
-      }
-    ]
-  });
+router.get('/', async (req, res) => {
+  try {
+    const categories = await Category.findAll({
+      include: [
+        {
+          model: Nominee,
+          include: [
+            {
+              model: Movie,
+            },
+          ],
+        },
+      ],
+    });
+
+    res.json(categories);
+  } catch (err) {
+    console.error(err);
+  }
 });
+
+module.exports = router;
