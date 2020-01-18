3b6de720da7ed1a4d0fc854dbe60d71d139db6b1
Dante Tobar <dante.m.tobar@gmail.com>
2020-01-18 11:48:27 -0800

    client/app.js

--- client/app.js
+++ client/app.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios';
 
+import Routes from './routes';
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      user: "",
+      user: '',
       choices: [],
-      nominees: [],
-      step: 0
+      categories: [],
     };
   }
+
+  async componentDidMount() {
+    const { data } = await axios.get('/api/category');
+    this.setState({
+      categories: data,
+    });
+  }
+
   render() {
-    return <div id="mount"></div>;
+    const categories = this.state.categories || [];
+    return (
+      <div id="mount">
+        <Routes categories={categories} />
+      </div>
+    );
   }
 }
 
