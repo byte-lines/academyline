e178ff0753c3f0ac16a0dd1c4314bb93bed63514
Dante Tobar <dante.m.tobar@gmail.com>
2020-01-21 12:46:34 -0800

    added session

--- server/index.js
+++ server/index.js
@@ -1,16 +1,50 @@
+const PORT = process.env.PORT || 8080;
+
 const express = require('express');
 const app = express();
 const path = require('path');
 const morgan = require('morgan');
 const { db } = require('./db');
 
-const PORT = process.env.PORT || 8080;
+// SESSION AND USER AUTHENTICATION
+const session = require('express-session');
+const passport = require('passport');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sessionStore = new SequelizeStore({ db });
+
+if (process.env.NODE_ENV === 'test') {
+  after('close the session store', () => sessionStore.stopExpiringSessions());
+}
+
+// passport registration
+passport.serializeUser((user, done) => done(null, user.id));
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.models.user.findByPk(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
 
 const createApp = () => {
   app.use(morgan('dev'));
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+
+  // session middleware with passport
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET || 'my best friend is Cody',
+      store: sessionStore,
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   app.use('/api', require('./api'));
 
