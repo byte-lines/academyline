4349542fecdfe179e75f4ebba9d480bed0260736
Dante Tobar <dante.m.tobar@gmail.com>
2020-02-09 11:13:33 -0800

    topbar has links

--- client/components/TopBar.js
+++ client/components/TopBar.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 const TopBar = props => {
-  return <div id="top-bar" className={props.unload ? 'unload' : ''} />;
+  const navTo = page => {
+    props.history.push(page);
+  };
+  return (
+    <div id="top-bar" className={props.unload ? 'unload' : ''}>
+      <h2 onClick={() => navTo('/summary')}>Summary</h2>
+      <h2 onClick={() => navTo('/leaderboard')}>Leaderboard</h2>
+      <h2 onClick={() => navTo('/survey')}>Survey</h2>
+    </div>
+  );
 };
 
-export default TopBar;
+export default withRouter(TopBar);
