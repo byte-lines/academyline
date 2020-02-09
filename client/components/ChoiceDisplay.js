bd7e72332954bb2fd9a56d58eee391bdbdd26ec1
Henry Griffith <hgriffithdev@gmail.com>
2020-02-09 13:43:46 -0800

    tweaked user info on choice display, added styling to summarylist

--- client/components/ChoiceDisplay.js
+++ client/components/ChoiceDisplay.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const ChoiceDisplay = props => {
   const { category, totalAnswers } = props;
@@ -9,14 +9,22 @@ const ChoiceDisplay = props => {
       className="summary-row"
       onClick={() => handleClick(category.id)}
     >
-      <h1 className="nominee">{category.choice && category.choice.name}</h1>
-      <div>
-        <p>{category.choice && category.choice.movie.title}</p>
+      <h1 className="nominee">
+        <span>{category.pseudoId}.</span>{" "}
+        {category.choice && category.choice.name}
+      </h1>
+      <div className="choice-info">
+        <p>
+          {category.choice && category.choice.movie.title}{" "}
+          <span>
+            Chosen by{" "}
+            {`${Math.round(
+              (100 * category.choice.users.length) / totalAnswers
+            )}%`}
+          </span>
+        </p>
         <p>{category.name}</p>
       </div>
-      <h4>
-        {`${Math.round((100 * category.choice.users.length) / totalAnswers)}%`}
-      </h4>
     </div>
   );
 };
